e4001bcabaf88e6e2c158a89ccb7077c0441df63
Sri014 <121746552+Sri014@users.noreply.github.com>
2024-04-19 20:54:58 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -214,8 +214,8 @@ const generateM3u = async (ud) => {
             //m3uStr += chansList[i].channel_url + '\n\n';
            const playUrl = replacestrings(`https://beta-ts-sable.vercel.app/index.mpd?id=${chansList[i].channelMeta.id}`);
 
-                        //m3uStr += playUrl + '\n\n';
-                    m3uStr += replacestrings(`https://rftv.wtf/tp/tplay.mpd?contentID=${chansList[i].channel_id}`) + '\n\n';
+                        m3uStr += playUrl + '\n\n';
+                    //m3uStr += replacestrings(`https://rftv.wtf/tp/tplay.mpd?contentID=${chansList[i].channel_id}`) + '\n\n';
         }
         console.log('all done!');
     } else {
